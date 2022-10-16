d35713a9c76107db82506b9b73da71b0582ab7c8
Ysmael Trias Jr <113600428+ysmaeltrias@users.noreply.github.com>
2022-10-16 11:33:31 +0800

    Add files via upload

---
+++ Exercise.py
@@ -0,0 +1,7 @@
+def full_name(str):   
+    str = str[::-1]   
+    return str   
+    
+name = "Ysmael Trias Jr"  
+print ("The original string is: ", name)   
+print ("The reversed string is: ", full_name(name))
